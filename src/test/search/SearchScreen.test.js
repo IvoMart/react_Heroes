1b4181422370bda5ee2dab40944eefd9658980f0
IvoMart <ivo_ivancho@hotmail.com>
2022-01-27 16:34:16 -0300

    SearchScreen test 2

--- src/test/search/SearchScreen.test.js
+++ src/test/search/SearchScreen.test.js
@@ -17,4 +17,25 @@ describe('Pruebas en <SearchScreen />', () => {
     expect(wrapper.find('.alert-info').text().trim()).toBe('Buscar un Heroe');
   });
   
+  test('Debe mostrar a Batman y el input con el valor del query String', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=batman']}>
+        <SearchScreen/>
+      </MemoryRouter>
+    )
+
+    expect(wrapper.find('input').prop('value')).toBe('batman');
+  });
+ 
+  test('Debería de mostrar un error si no se encuentra resultado', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search?q=batman13546']}>
+        <SearchScreen/>
+      </MemoryRouter>
+    )
+  expect(wrapper.find('.alert-danger').text().trim()).toBe('No hay registro para batman13546');
+  });
+  
+
+
 });
