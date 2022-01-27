bc4d3947d2399cebcd9efa8bc565654cb1a4c51d
IvoMart <ivo_ivancho@hotmail.com>
2022-01-27 17:03:08 -0300

    SearchScreen test 3

--- src/test/search/SearchScreen.test.js
+++ src/test/search/SearchScreen.test.js
@@ -3,39 +3,71 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { SearchScreen } from '../../components/search/SearchScreen';
 
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => (
+  { 
+    //@ts-ignore
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate,
+  }
+))
+
+
 describe('Pruebas en <SearchScreen />', () => {
   test('Debe mostrarse con valores por defecto', () => {
-    
+
     const wrapper = mount(
       <MemoryRouter initialEntries={['/search']}>
-        <SearchScreen/>
+        <SearchScreen />
       </MemoryRouter>
     )
-    
+
     expect(wrapper).toMatchSnapshot();
 
     expect(wrapper.find('.alert-info').text().trim()).toBe('Buscar un Heroe');
   });
-  
+
   test('Debe mostrar a Batman y el input con el valor del query String', () => {
     const wrapper = mount(
       <MemoryRouter initialEntries={['/search?q=batman']}>
-        <SearchScreen/>
+        <SearchScreen />
       </MemoryRouter>
     )
 
     expect(wrapper.find('input').prop('value')).toBe('batman');
   });
- 
+
   test('Debería de mostrar un error si no se encuentra resultado', () => {
     const wrapper = mount(
       <MemoryRouter initialEntries={['/search?q=batman13546']}>
-        <SearchScreen/>
+        <SearchScreen />
       </MemoryRouter>
     )
-  expect(wrapper.find('.alert-danger').text().trim()).toBe('No hay registro para batman13546');
+    expect(wrapper.find('.alert-danger').text().trim()).toBe('No hay registro para batman13546');
   });
-  
 
 
+  test('Debe cambiar de ruta por Navigate', () => {
+
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/search']}>
+        <SearchScreen />
+      </MemoryRouter>
+    )
+
+    wrapper.find('input').simulate('change', {
+      target: {
+        name: 'searchText',
+        value: 'batman'
+      }
+    })
+
+    //@ts-ignore
+    wrapper.find('form').prop('onSubmit')({preventDefault(){}})
+
+    expect(mockNavigate).toHaveBeenCalled();
+
+    expect(mockNavigate).toHaveBeenCalledWith('?q=batman');
+  });
 });
