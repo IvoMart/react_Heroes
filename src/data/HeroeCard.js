a652213835b487930a06e3b102c77c6ce6e5fabd
IvoMart <ivo_ivancho@hotmail.com>
2022-01-28 14:40:07 -0300

    Referencia de imagenes relativa y como Helper

--- src/data/HeroeCard.js
+++ src/data/HeroeCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { imagesPath } from '../helpers/sourceImages';
 
 export const HeroeCard = ({
   id,
@@ -10,13 +11,14 @@ export const HeroeCard = ({
   characters,
 }) => {
 
-  const imgagePath = `/assets/${id}.jpg`
+  // const imagePath = require(`../assets/${id}.jpg`)
+  const imagePath = imagesPath(id);
 
   return (
     <div className="animate__animated animate__bounceIn">
       <div className='card'>
         <Link to={`/heroe/${id}`} >
-          <img src={imgagePath} alt={superhero} className='card-img' />
+          <img src={imagePath} alt={superhero} className='card-img' />
         </Link>
         <div className="card-body">
           <h4 className='card-title text-uppercase' >{superhero}</h4>
